269e8a8760511a03f0bdf87f5f1f5d6983c557b3
Vishwas Paikra <vishwaspaikra007@gmail.com>
2022-05-29 16:55:36 +0530

    Updates

---
+++ precache-manifest.1fbc62eae00794cc1fe54a6c3545ba24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cc4305b5cc69f917a7251819f778482",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "08bc91901e9de48a4634",
+    "url": "/portfolio/static/css/main.0e39dd0e.chunk.css"
+  },
+  {
+    "revision": "e3fe68056905d803d694",
+    "url": "/portfolio/static/js/2.e36d33f5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.e36d33f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08bc91901e9de48a4634",
+    "url": "/portfolio/static/js/main.0c84a70c.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
